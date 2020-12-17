56bdc7fc9e4221e886eb2f400ab786e6587a531e
Nick <jmfxxx@gmail.com>
2020-12-17 14:51:29 +0800

    新增使用者權限列表頁面

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h3>使用者權限頁面</h3>
+
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
